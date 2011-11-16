fad73101635a960577ba7365e8c0de72d178b304
= <=>
2011-11-16 16:07:42 -0600

    edited the about page structure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def about
+    @title = "about"
   end
 
   def help
+    @title = "help"
   end
 
   def contact
+    @title = "contact"
   end
 
 end
