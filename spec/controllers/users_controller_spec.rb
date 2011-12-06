bc0010bcd4468f113651b1b3dbc2a3db8e150122
= <=>
2011-12-06 15:26:33 -0600

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -33,7 +33,58 @@ describe UsersController do
       get :show, :id => @user
       response.should have_selector("h1>img", :class => "gravitar")
     end
+  end
+  
+  
+  
+  describe "POST 'create" do
+    describe "failure" do
+      before(:each) do
+       @attrib = (:name => "", :email => "", :password => "", :password_confirmation => "")
+      end
+      
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attrib
+          end.should_not change(User, :count)
+    end
     
+    
+    it "should have the right title" do
+    post :create, :user => @attrib
+    response.should have_selector("title", :content => "Sign up")
+    end
+    
+    it "should render the 'new' page" do
+    post :create, :user => @attrib
+    response.should render_template('new')
+    end 
+  end
+  
+  
+   describe "success" do
+      before(:each) do
+       @attrib = (:name => "New User", :email => "user@kommingle.com", :password => "foobar", :password_confirmation => "foobar")
+      end
+      
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attrib
+        end.should change(User, :count).by(1)
+    end
+    
+
+    it "should redirect to the user show page" do
+    post :create, :user => @attrib
+    response.should redirect_to(user_path(assigns(:user)))
+    end 
+    
+      it "should have a welcome message" do
+    post :create, :user => @attrib
+    flash[:success].should =~/welcome to the sample app/i
+    end 
+  end
+  
   end
   
 end
